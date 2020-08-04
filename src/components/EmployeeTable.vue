5a30ba6c8d636e28e2dc4618b8f877e7287c1c70
crevulus <crevulus@gmail.com>
2020-08-04 22:08:12 +0200

    init vue; rendering table

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="EmployeeTable">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="e in employees" v-bind:key="e.id">
+          <td>{{e.name}}</td>
+          <td>{{e.email}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmployeeTable",
+  props: {
+    employees: Array,
+  },
+};
+</script>
+
+<style scoped></style>
