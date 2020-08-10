8bc11147f9d0c4a06944cdd450e5cc24d5885d89
crevulus <crevulus@gmail.com>
2020-08-10 17:01:33 +0100

    imported assets

--- src/pages/Maria.vue
+++ src/pages/Maria.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
     <h2>Maria</h2>
+    <img src="{{images[0]}}" />
   </div>
 </template>
+<script>
+import arrow from "../assets/arrow.jpg";
+export default {
+  name: "Maria",
+  data() {
+    return {
+      images: [arrow],
+    };
+  },
+};
+</script>
+<style></style>
