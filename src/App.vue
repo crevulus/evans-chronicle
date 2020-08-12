36e573a3ed7abe67197a83f086dcf05d86c103ad
crevulus <crevulus@gmail.com>
2020-08-12 10:14:56 +0200

    added vuetify"

--- src/App.vue
+++ src/App.vue
@@ -15,27 +15,6 @@ export default {
     // EmployeeTable,
     NavBar,
   },
-  data() {
-    return {
-      employees: [
-        {
-          id: 1,
-          name: "Richard Hendricks",
-          email: "richard@piedpiper.com",
-        },
-        {
-          id: 2,
-          name: "Bertram Gilfoyle",
-          email: "gilfoyle@piedpiper.com",
-        },
-        {
-          id: 3,
-          name: "Dinesh Chugtai",
-          email: "dinesh@piedpiper.com",
-        },
-      ],
-    };
-  },
 };
 </script>
 
