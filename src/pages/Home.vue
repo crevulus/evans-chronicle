6252c75dae07826b15aa7e820cb3356647f382e5
crevulus <crevulus@gmail.com>
2020-08-25 12:01:22 +0200

    getting card data onto home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,45 +1,42 @@
 <template>
   <div>
     <h2>Home</h2>
-    <Cards v-if="dataLoaded" v-bind:imageData="this.pictures" />
+    <div v-if="this.dataLoaded" class="cards-container">
+      <div v-for="picture in picturesData" :key="picture.id" class="card">
+        <img :src="picture.source" :alt="picture.caption" class="card-img" />
+        <div class="text-box">
+          <p>{{ moment(picture.timestamp).format("MMM Do YYYY") }}</p>
+          <p>{{ picture.caption }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 import { db } from "../db";
-import Cards from "../components/Cards";
 
 export default {
-  components: {
-    Cards,
-  },
-  data() {
-    return {
-      test: "test",
-      family: "",
-      pictures: null,
-      dataLoaded: false,
-    };
-  },
-  // created() {
-  //   getData()
-  //     .then((data) => JSON.stringify(data))
-  //     .then((d) => JSON.parse(d))
-  //     .then((item) => {
-  //       this.pictures = item.pictures;
-  //       this.dataLoaded = true;
-  //     });
-  //   console.log("h");
-  // },
+  components: {},
+  data: () => ({
+    test: "test",
+    family: "",
+    dataLoaded: true,
+    picturesData: [],
+  }),
   mounted() {
     this.getEvents();
   },
   methods: {
     async getEvents() {
       // instead of using promises/thens
-      let snapshot = await db.collection("Maria").get();
+      let snapshot = await db.collection("Maria").limit(1).get();
+      let firstPicturesData;
       snapshot.forEach((doc) => {
-        console.log(doc.data());
+        let appData = doc.data();
+        appData.id = doc.id;
+        firstPicturesData = appData;
       });
+      this.picturesData.push(firstPicturesData);
     },
   },
 };
